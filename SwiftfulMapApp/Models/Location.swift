95632773556af8e67c758625ef755e49de84800b
Marcio Curvello <marciocurvello@gmail.com>
2024-03-10 21:57:28 -0300

    add models

---
+++ SwiftfulMapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwiftfulMapApp
+//
+//  Created by Marcio Curvello on 10/03/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
