b8ffeb3c8b891325a70f71acbaf038609f82c17f
Marcio Curvello <marciocurvello@gmail.com>
2024-03-10 21:44:02 -0300

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Marcio Curvello on 10/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
